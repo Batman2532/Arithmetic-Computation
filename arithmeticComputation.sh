1497014cd1ece1e27f49179dd8885be2d503e724
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-08 14:43:47 +0530

    Added: read the values from dictionary to array

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -6,4 +6,9 @@ compute["a+b*c"]=$(( $a + ($b  * $c )))
 compute["a*b+c"]=$((( $a * $b ) + $c ))
 compute["c+a/b"]=$(( $c + ( $a  / $b )))
 compute["a%b+c"]=$((( $a % $b ) +  $c ))
-echo ${compute[@]}
+i=0
+for key in "${!compute[@]}";
+do
+	array[((i++))]=$((compute[$key]))
+done
+echo ${array[@]}
