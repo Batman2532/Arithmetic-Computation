6d7270479bcef29047a0e004d0bd7032f8a555b6
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-08 15:09:20 +0530

    Added: sort array in decending order

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 read -p "Enter values fro a, b & c:" a b c
 declare -A compute
@@ -6,9 +6,25 @@ compute["a+b*c"]=$(( $a + ($b  * $c )))
 compute["a*b+c"]=$((( $a * $b ) + $c ))
 compute["c+a/b"]=$(( $c + ( $a  / $b )))
 compute["a%b+c"]=$((( $a % $b ) +  $c ))
-i=0
+a=0
 for key in "${!compute[@]}";
 do
-	array[((i++))]=$((compute[$key]))
+	array[((a++))]=$((compute[$key]))
 done
-echo ${array[@]}
+
+for ((i=0; i<4; i++))
+do
+	for ((j=0; j<4-i-1;i++))
+	do
+		if [ ${array[j]} -gt ${array[$((j+1))]} ]
+        then
+            temp=${array[j]}
+            array[$j]=${array[$((j+1))]}  
+            array[$((j+1))]=$temp
+        fi
+    done
+done
+for ((k=a-1; k>=0; k--))
+do
+echo ${array[k]}
+done
