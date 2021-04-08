7e907d74e4ebb5060ba9b3df40c6c9cedf68c39e
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-08 14:20:13 +0530

    Added: store result in dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
 read -p "Enter values fro a, b & c:" a b c
-echo "first compute: " $(( $a + ($b  * $c )))
-echo "second compute: "$((( $a * $b ) + $c ))
-echo "third compute: "$(( $c + ( $a  / $b )))
-echo "third compute: "$((( $a % $b ) +  $c ))
+declare -A compute
+compute["a+b*c"]=$(( $a + ($b  * $c )))
+compute["a*b+c"]=$((( $a * $b ) + $c ))
+compute["c+a/b"]=$(( $c + ( $a  / $b )))
+compute["a%b+c"]=$((( $a % $b ) +  $c ))
+echo ${compute[@]}
