359a497ee78c64e561d7771d0d7ac9180fcf9332
Batman2532 <73815402+Batman2532@users.noreply.github.com>
2021-04-08 14:02:40 +0530

    Added: compute: a%b+c

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,4 +4,4 @@ read -p "Enter values fro a, b & c:" a b c
 echo "first compute: " $(( $a + ($b  * $c )))
 echo "second compute: "$((( $a * $b ) + $c ))
 echo "third compute: "$(( $c + ( $a  / $b )))
-
+echo "third compute: "$((( $a % $b ) +  $c ))
